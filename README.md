8d4afd7c7cb577624d30e0c06232db5c9ce0658f
Jake Lazaroff <yo@jake.nyc>
2022-12-28 17:55:11 -0500

    Add script to update README with index of TILs

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Today I Learned
 
 A collection of useful things I've learned. Inspired by [simonw/til](https://github.com/simonw/til).
+
+---
+
+## docker
+
+- [Add root certificates to a Debian container](/docker/add-root-certificates-to-a-debian-container.md)
+
+## entr
+
+- [Rerun a command when files change](/entr/rerun-a-command-when-files-change.md)
---
+++ readme.js
@@ -0,0 +1,57 @@
+#!/usr/bin/env node
+
+const { execSync } = require("node:child_process");
+const { readFile, writeFile } = require("node:fs/promises");
+
+function exec(cmd) {
+  return execSync(cmd).toString();
+}
+
+async function main() {
+  console.log("Building index…");
+
+  const files = exec("git ls-files")
+    .split("\n")
+    .filter((name) => name.includes("/"))
+    .sort()
+    .map((name) => name.split("/"));
+
+  const tils = new Map();
+  for (const [category, file] of files) {
+    const list = tils.get(category) || [];
+    tils.set(category, [...list, file]);
+  }
+
+  let readme = await loadReadmeHeader("README.md");
+  for (const [category, files] of tils.entries()) {
+    readme += `\n## ${category}\n\n`;
+
+    for (const file of files) {
+      const filepath = [category, file].join("/");
+      const { title } = await loadTil(filepath);
+      readme += `- [${title}](/${filepath})\n`;
+    }
+  }
+
+  await writeFile("README.md", readme);
+  exec("git add README.md");
+}
+
+async function loadReadmeHeader(filepath) {
+  const contents = await readFile(filepath).then((file) => file.toString());
+
+  const needle = "\n---\n";
+  const index = contents.indexOf(needle);
+  if (index === -1) return contents + needle;
+
+  return contents.substring(0, index + needle.length);
+}
+
+async function loadTil(filepath) {
+  const contents = await readFile(filepath).then((file) => file.toString());
+  const [, title] = contents.match(/^# (.+)$/m);
+
+  return { title };
+}
+
+main();
